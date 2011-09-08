a024d7a1b9766ffa4769d7cfe20b04012a49815b
colinmccann <colindmccann@gmail.com>
2011-09-08 05:57:05 -0400

    sort of working with youtube pulls

--- js/scihub.js
+++ js/scihub.js
@@ -54,18 +54,7 @@ SciHub = {
         }
     },
     
-    showMyVideos: function(data) {
-  	  var feed = data.feed;
-  	  var entries = feed.entry || [];
-  	  var html = ['<ul>'];
-  	  for (var i = 0; i < entries.length; i++) {
-  	    var entry = entries[i];
-  	    var title = entry.title.$t;
-  	    html.push('<li>', title, '</li>');
-  	  }
-  	  html.push('</ul>');
-  	  document.getElementById('videos').innerHTML = html.join('');
-    }, 
+
     
     events: {
         sail: {
