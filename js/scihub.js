16334096958a9e016d875f7ad4f7376af63f89c5
Matt Zukowski <matt@zukowski.ca>
2011-11-07 01:12:18 -0500

    whole bunch of usability/UI improvements

--- js/scihub.js
+++ js/scihub.js
@@ -1,100 +1,157 @@
-//    DRONNING MAUDS GATE 10-11
-//    ENTRANCE FROM MUNKEDAMSVEIEN
+/*jslint regexp: false, browser: true */
+/*globals $, Sail, Rollcall, swfobject */
 
 
-SciHub = {
+var SciHub = {
     rollcallURL: '/rollcall', //'http://rollcall.proto.encorelab.org',
-    xmppDomain: 'imediamac28.uio.no',
     groupchatRoom: 'scihub@conference.imediamac28.uio.no',
     
     init: function() {
-        console.log("Initializing...")
+        console.log("Initializing...");
+        
+        // this is currently unused in SciHub
+        Sail.app.run = JSON.parse($.cookie('run'));
         
         Sail.modules
             .load('Rollcall.Authenticator', {mode: 'multi-picker'})
             .load('Strophe.AutoConnector')
             .load('AuthStatusWidget')
             .thenRun(function () {
-                Sail.autobindEvents(SciHub)
+                Sail.autobindEvents(SciHub);
                 
                 $(document).ready(function() {
                     $('#reload').click(function() {
-                        console.log("Disconnecting...")
-                        Sail.Strophe.disconnect()
-                        console.log("Reloading "+location+"...")
-                        location.reload()
-                    })
-
-                    $('#connecting').show()
-                    
-                })
+                        console.log("Disconnecting...");
+                        Sail.Strophe.disconnect();
+                        console.log("Reloading "+location+"...");
+                        location.reload();
+                    });
+                });
                 
-                $(Sail.app).trigger('initialized')
-                return true
-            })
+                $(Sail.app).trigger('initialized');
+                return true;
+            });
     },
    
     authenticate: function() {
-        console.log("Authenticating...")
+        console.log("Authenticating...");
         
-        SciHub.rollcall = new Rollcall.Client(SciHub.rollcallURL)
-        SciHub.token = SciHub.rollcall.getCurrentToken()
+        SciHub.rollcall = new Rollcall.Client(SciHub.rollcallURL);
+        SciHub.token = SciHub.rollcall.getCurrentToken();
 
         if (!SciHub.token) {
-            Rollcall.Authenticator.requestLogin()
+            Rollcall.Authenticator.requestLogin();
         } else {
             SciHub.rollcall.fetchSessionForToken(SciHub.token, function(data) {
-                SciHub.session = data.session
-                $(SciHub).trigger('authenticated')
-            })
+                SciHub.session = data.session;
+                $(SciHub).trigger('authenticated');
+            });
         }
     },
     
     createVideoPlayer: function(url) {
-        player = $("<object class='player'></object>")
-        player.append("<param name='movie' value='"+url+"'></param>")
-        player.append("<param name='allowFullScreen' value='true'></param>")
-        player.append("<param name='allowScriptAccess' value='always'></param>")
-        player.append("<embed class='player' src='"+url+"' type='application/x-shockwave-flash' allowfullscreen='true' allowscriptaccess='always'></embed>")
+        var videoId;
+        var videoIdMatch = url.match(new RegExp('http://(?:www.)?youtube.com/v/([^?&]*)'));
+        if (!videoIdMatch || !videoIdMatch[1]) {
+            throw "Video url '"+url+"' could not be parsed to retrieve the video id!";
+        } else {
+            videoId = videoIdMatch[1];
+        }
         
-        return player
+        // AS3 CHROMELESS PLAYER
+        //        var yurl = "http://www.youtube.com/apiplayer?video_id="+video_id+"&version=3&showinfo=0&enablejsapi=1";
+        //        var player = $("<object id='video-"+video_id+"' class='player'></object>")
+        //                        .append("<param name='movie' value='"+yurl+"'></param>")
+        //                        .append("<param name='allowFullScreen' value='true'></param>")
+        //                        .append("<param name='allowScriptAccess' value='always'></param>")
+        //                        .append("<embed class='player' src='"+yurl+"' type='application/x-shockwave-flash' allowfullscreen='true' allowscriptaccess='always'></embed>");               
+        
+        
+        // AS3 CHROMELESS PLAYER USING swfobject.js
+        var playerId = "video-player-"+videoId+'-'+Math.round(Math.random()*10e12).toString(32);
+        var player = $('<div />').attr('id', playerId);
+        player.data('video-id', videoId);
+        player[0].loadVideo = function() {
+            var playerId = $(this).attr('id');
+            var videoId = $(this).data('video-id');            
+            console.log("Loading video "+videoId);
+            var params = { allowScriptAccess: "always", allowFullScreen: 'true' };
+            var atts = { id: playerId };
+            var yurl = "http://www.youtube.com/apiplayer?video_id="+videoId+"&version=3&showinfo=0&enablejsapi=1&playerapiid="+playerId;
+            swfobject.embedSWF(yurl, playerId, "100%", "100%", "8", null, null, params, atts);
+            
+            var overlay = $("<div class='video-overlay'>");
+            $('#'+playerId).after(overlay);
+            
+            overlay.dblclick(function() {
+                var player = $(this).prevAll('object')[0];
+                if (player.getPlayerState() === 1) {
+                    player.pauseVideo();
+                } else {
+                    player.playVideo();
+                }
+            });
+            
+        };
+        
+        
+    
+        // AS3 PLAYER
+        // var yurl = url + "&version=3"
+        // var player = $("<object class='player'></object>")
+        //             .append("<param name='movie' value='"+yurl+"'></param>")
+        //             .append("<param name='allowFullScreen' value='true'></param>")
+        //             .append("<param name='allowScriptAccess' value='always'></param>")
+        //             .append("<param name='autohide' value='1'></param>")
+        //             .append("<param name='egm' value='0'></param>")
+        //             .append("<param name='rel' value='0'></param>")
+        //             .append("<embed class='player' src='"+yurl+"' type='application/x-shockwave-flash' rel='0' allowfullscreen='true' allowscriptaccess='always' egm='0' autohide='1'></embed>");
+        // 
+        
+                
+        // ALTERNATIVE PLAYER USES IFRAME TO DISPLAY <video> tag
+        // var player = $("<iframe type='text/html' frameborder='0' />")
+        //                 .attr('src', url.replace('http://www.youtube.com/v/', 'http://www.youtube.com/embed/'))
+                 
+        return player;
     },
     
     createVideoBalloon: function(token, url) {
-        balloon = $("<div class='balloon video' ></div>")
+        var player, loader;
+        var balloon = $("<div class='balloon video' ></div>");
+        
+        
+        balloon.addClass('token-' + token);
         
         if (url) {
-            player = SciHub.createVideoPlayer(url)
+            player = SciHub.createVideoPlayer(url);
             
-            balloon.append(player)
+            balloon.append(player);
         } else {
-            loader = $("<div class='loader'></div>")
-            loader.append("<img src='loader.gif' />")
-            loader.append("<p>Processing...</p>")
+            loader = $("<div class='loader'></div>");
+            loader.append("<img src='loader.gif' />");
+            loader.append("<p>Processing...</p>");
             
-            balloon.append(loader)
+            balloon.append(loader);
         }
         
-        
-        balloon.addClass('token-' + token)
-        
-        balloon.mouseover(function() {
-            $(this).css({width: 360, height: 270})
-        })
-        
-        balloon.mouseout(function() {
-            $(this).css({width: 240, height: 180})
-        })
+        // balloon.mouseover(function() {
+        //             $(this).css({width: 360, height: 270});
+        //         });
+        //         
+        //         balloon.mouseout(function() {
+        //             $(this).css({width: 240, height: 180});
+        //         });
         
         //balloon.addClass('author-'+author.replace(/[^a-z0-9]/i, '-'))
         
-        balloon.hide()
+        balloon.hide();
         
-        wall_height = $("#wall").height()
-        wall_width = $("#wall").width()
+        var wall_height = $("#wall").height();
+        var wall_width = $("#wall").width();
         
-        balloon.css('left', (Math.random() * (wall_width - 100) + 'px'))
-        balloon.css('top', (Math.random() * (wall_height - 100) + 'px'))
+        balloon.css('left', (Math.random() * (wall_width - 100) + 'px'));
+        balloon.css('top', (Math.random() * (wall_height - 100) + 'px'));
         
         // balloon.dblclick(function() {
         //     expl = $(this).find('.explanation')
@@ -104,109 +161,130 @@ SciHub = {
         //         $(expl).show('slide', {direction: 'up', duration: 'fast'})
         // })
         
-        $("#wall").append(balloon)
+        $("#wall").append(balloon);
         
-        balloon.show('puff', 'fast')
+        balloon.show('puff', 'fast');
         balloon.mousedown(function() {
-            zs = $('.balloon').map(function() {z = $(this).css('z-index'); return z == 'auto' ? 100 : parseInt(z)})
-            maxZ = Math.max.apply(Math, zs)
-            $(this).css('z-index', maxZ + 1)
-        })
+            var zs = $('.balloon').map(function() {
+                var z = $(this).css('z-index');
+                return (z === 'auto') ? 100 : parseInt(z, 10);
+            }).toArray();
+            var maxZ = Math.max.apply(Math, zs);
+            $(this).css('z-index', maxZ + 1);
+        });
         
-        balloon.draggable()
-        return balloon
+        balloon.draggable();
+        
+        if (player && player[0]) {
+            player[0].loadVideo();
+        }
+        
+        return balloon;
     },
     
 
     
     events: {
         sail: {
-        	got_google_client_token: function(sev) {
-        		tokenURL = sev.payload.url
-        		token = sev.payload.token
-        		
-        		nexturl = escape(document.location.href.replace(/\?.*/,'') + '#token='+token)
-        		$('#submission-form').attr('action', tokenURL+"?nexturl="+nexturl)        		
+            got_google_client_token: function(sev) {
+                var tokenURL = sev.payload.url;
+                var token = sev.payload.token;
+                
+                var nexturl = encodeURI(document.location.href.replace(/\?.*/,'') + '#token='+token);
+                $('#submission-form').attr('action', tokenURL+"?nexturl="+nexturl);    
                 //$('#submission-form').get(0).setAttribute('action', tokenURL+"?nexturl="+nexturl);
-                $('#token-value').val(token)
-                console.log(token)
-                console.log(tokenURL)
-                Sail.app.createVideoBalloon(token)
-                console.log("video created")
+                $('#token-value').val(token);
+                console.log(token);
+                console.log(tokenURL);
+                Sail.app.createVideoBalloon(token);
+                console.log("video created");
             },
             
             video_ready: function(sev) {
-            	videoReadyURL = sev.payload.url
-            	token = sev.payload.token
-            	
-            	console.log(videoReadyURL)
-            	$('.token-'+token+' .loader').remove()
-            	
-            	player = SciHub.createVideoPlayer(videoReadyURL)
-            	$('.token-'+token).append(player)
+                var videoReadyURL = sev.payload.url;
+                var token = sev.payload.token;
+                
+                console.log(videoReadyURL);
+                $('.token-'+token+' .loader').remove();
+                
+                var player = SciHub.createVideoPlayer(videoReadyURL);
+                $('.token-'+token).append(player);
+                player[0].loadVideo();
             }
    
         },
         
         initialized: function(ev) {
-            SciHub.authenticate()
+            SciHub.authenticate();
         },
         
         connected: function(ev) {
-            SciHub.groupchat.join()
-            $('#username').text(session.account.login)					
-        	
+            
             //init VIEW screen
             
             
             $('.upload-screen-button').click(function() {
-            	sev = new Sail.Event('video_upload_requested',{})
-            	SciHub.groupchat.sendEvent(sev)
-            	//$('#view').hide()
-            	Sail.UI.showDialog("#add")
-            	$('#add .ui-dialog-titlebar-close')
-            	    .click(function() {Sail.UI.dismissDialog("#add")})
-            	    .mouseover(function() {$(this).addClass('ui-state-hover')})
-                    .mouseout(function() {$(this).removeClass('ui-state-hover')})
-            })
+                var sev = new Sail.Event('video_upload_requested',{});
+                SciHub.groupchat.sendEvent(sev);
+                //$('#view').hide()
+                Sail.UI.showDialog("#add");
+                $('#submission-form')
+                    .bind('submit', function(){
+                        if ($('#file').attr('value')) {
+                            $('#submit-upload')
+                                .attr('value', "Uploading...")
+                                .attr('disabled','disabled');
+                            Sail.Strophe.disconnect()
+                            return true;
+                        } else {
+                            alert("Select a file to upload first!")
+                            return false;
+                        }
+                    });
+                $('#add .ui-dialog-titlebar-close')
+                    .click(function() {Sail.UI.dismissDialog("#add");})
+                    .mouseover(function() {$(this).addClass('ui-state-hover');})
+                    .mouseout(function() {$(this).removeClass('ui-state-hover');});
+            });
             
             $('.upload-screen-button').show('fade');
             
-            $('#connecting').hide()	
 
             //init ADD screen
             
             $().sleepyMongo({sleepyUrl:'/mongoose/scihub/videos/'}).find({}, function(data){
                 $(data.results).each(function() {
-                    Sail.app.createVideoBalloon('', this.url)
-                })
-            })
+                    Sail.app.createVideoBalloon('', this.url);
+                });
+            });
 
 
-            id = ''
-            idMatch = document.location.href.match(/id=([^&#]+)/)
-            if (idMatch)
-            	id = idMatch[1]
+            var id = '';
+            var idMatch = document.location.href.match(/id=([^&#]+)/);
+            if (idMatch) {
+                id = idMatch[1];
+            }
 
-            status = ''
-            statusMatch = document.location.href.match(/status=([^&#]+)/)
-            if (statusMatch)
-            	status = statusMatch[1]
+            var status = '';
+            var statusMatch = document.location.href.match(/status=([^&#]+)/);
+            if (statusMatch) {
+                status = statusMatch[1];
+            }
             
-            token = ''
-            tokenMatch = document.location.href.match(/token=([^&#]+)/)
-            if (tokenMatch)
-            	token = tokenMatch[1]
-
-
-            if (status == '200') {
-            	sev = new Sail.Event('video_uploaded',{
-            		id:id,
-            		status:status,
-            		token:token
-            	})
-            	Sail.app.createVideoBalloon(token)
-            	SciHub.groupchat.sendEvent(sev)
+            var token = '';
+            var tokenMatch = document.location.href.match(/token=([^&#]+)/);
+            if (tokenMatch) {
+                token = tokenMatch[1];
+            }
+            
+            if (status === '200' || status === 200) {
+                var sev = new Sail.Event('video_uploaded',{
+                    id:id,
+                    status:status,
+                    token:token
+                });
+                Sail.app.createVideoBalloon(token);
+                SciHub.groupchat.sendEvent(sev);
             }
             
             
@@ -217,58 +295,7 @@ SciHub = {
         },
         
         logout: function(ev) {
-            Sail.app.run = null
+            Sail.app.run = null;
         }
     }    
-} 
-    /*    phonegap: function() {
-    $('#camera').click(function() {
-        navigator.camera.getPicture(onSuccess, onFail, { quality: 15 }); 
-
-        function onSuccess(imageData) {
-          var image = document.getElementById('photo');
-          image.src = "data:image/jpeg;base64," + imageData;
-        }
-
-        function onFail(message) {
-          alert('Failed because: ' + message);
-        }
-    })
-    
-    $('#phonegap-info').html(JSON.stringify(navigator.device).replace(/,/g,',<br />'))
-    
-    navigator.accelerometer.watchAcceleration(
-        function(acc) {
-            $('#accelerometer').text("x: "+acc.x+", y:"+acc.y+", z:"+acc.z)
-        }
-    )
-    
-    navigator.compass.watchHeading(
-        function(heading) {
-            $('#compass').text(heading)
-        }
-    )
-    
-    navigator.geolocation.watchPosition(
-        function(position) {
-            $('#geolocation').text("Lat: "+acc.coords.latitude+", Long:"+acc.coords.longitude)
-        }
-    )
-    
-    $('#alert').click(function() {
-        navigator.notification.alert("This is an alert!", null, "Uh oh!", "Okay")
-    })
-    
-    $('#confirm').click(function() {
-        navigator.notification.alert("This is a confirmation!", null, "Yay!", "Alright")
-    })
-    
-    $('#beep').click(function() {
-        navigator.notification.beep(3)
-    })
-    
-    $('#vibrate').click(function() {
-        navigator.notification.vibrate(1000)
-    })
-},
-*/    
+};
